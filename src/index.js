8b87466d14ebdce44395142da6fa70bba9437538
jk-rav <knudsen.jan@gmail.com>
2025-06-27 23:00:55 +0200

    added hosting

--- src/index.js
+++ src/index.js
@@ -8,23 +8,14 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
   apiKey: "AIzaSyDi3jkVvltaxKJbxbWt2SlwTum_dOk30rI",
-
   authDomain: "reno-checker.firebaseapp.com",
-
   projectId: "reno-checker",
-
   storageBucket: "reno-checker.firebasestorage.app",
-
   messagingSenderId: "852244918302",
-
   appId: "1:852244918302:web:fddd0f25909a8f8075be0f",
-
   measurementId: "G-78K0GETCGT"
-
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
