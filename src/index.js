256b3493d1749e194f2910bec65d463be999a62f
JKR <knudsen.jan@gmail.com>
2025-07-01 13:54:07 +0200

    update

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ console.log("testValue = " + myTestConst)
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyDi3jkVvltaxKJbxbWt2SlwTum_dOk30rI",
   authDomain: "reno-checker.firebaseapp.com",
@@ -31,6 +32,59 @@ const provider = new GoogleAuthProvider(auth);
 const db = getFirestore(app)
 
 
+
+function logInBtn() {
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage)
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+}
+
+function logOutBtn(){
+signOut(auth).then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
+}
+
+function setObserver(){
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    console.log("User is signed in with UID:", uid);
+    let myUser = ""
+    user.providerData.forEach((profile) => {
+    console.log("Sign-in provider: " + profile.providerId);
+    console.log("  Provider-specific UID: " + profile.uid);
+    console.log("  Name: " + profile.displayName);
+    console.log("  Email: " + profile.email);
+    console.log("  Photo URL: " + profile.photoURL);
+    myUser=profile.email
+    });
+  } else {
+    // User is signed out
+    console.log("No user is signed in.");
+  }
+});
+}
 
 console.log("testing auth")
 //function btnTest(){console.log("Testing Btn")}
